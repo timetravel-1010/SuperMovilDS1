bafc438f94857adfc7e8e593158965ce6c58b0a6
timetravel_1010 <timetravel_1010@protonmail.com>
2022-02-28 14:16:38 -0500

    se muestra bien la tabla.

--- src/vista/clientes/ModuloClientes.java
+++ src/vista/clientes/ModuloClientes.java
@@ -18,16 +18,14 @@ public class ModuloClientes extends javax.swing.JFrame {
      * Creates new form ModuloClientes
      */
     public ModuloClientes() {
+        
         initComponents();
-        panelTabla = new JPanelTablaClientes();
         this.setLocationRelativeTo(null);
-        this.pack();
-        this.revalidate();
-        this.repaint();
+        tablaC.setVisible(false);
     }
     
     public void pintarPanel(){
-        add(panelTabla, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 70, 400, 400));
+        add(panelTabla, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 20, 620, 256));
         panelTabla.agregarTodos();
         this.revalidate();
         this.repaint();
@@ -42,26 +40,42 @@ public class ModuloClientes extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        pintarTablaBtn = new javax.swing.JButton();
+        jPanelTablaClientes1 = new vista.clientes.JPanelTablaClientes();
+        jPanel1 = new javax.swing.JPanel();
+        panelInicial = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        tablaC = new vista.clientes.JPanelTablaClientes();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
-        pintarTablaBtn.setText("Pintar Tabla");
-        pintarTablaBtn.addActionListener(new java.awt.event.ActionListener() {
+        panelInicial.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                pintarTablaBtnActionPerformed(evt);
+                jButton1ActionPerformed(evt);
             }
         });
-        getContentPane().add(pintarTablaBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 10, -1, -1));
+        panelInicial.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(720, 40, -1, -1));
+        panelInicial.add(tablaC, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 40, 540, 270));
+
+        getContentPane().add(panelInicial, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 825, 600));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void pintarTablaBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pintarTablaBtnActionPerformed
-        // TODO add your handling code here:
-        pintarPanel();
-    }//GEN-LAST:event_pintarTablaBtnActionPerformed
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:        
+        //this.remove(panelInicial);
+        //this.revalidate();
+        //this.repaint();
+        //panelTabla = new JPanelTablaClientes();
+        //this.pintarPanel();
+        tablaC.setVisible(true);
+        tablaC.agregarTodos();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -99,6 +113,10 @@ public class ModuloClientes extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton pintarTablaBtn;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JPanel jPanel1;
+    private vista.clientes.JPanelTablaClientes jPanelTablaClientes1;
+    private javax.swing.JPanel panelInicial;
+    private vista.clientes.JPanelTablaClientes tablaC;
     // End of variables declaration//GEN-END:variables
 }
